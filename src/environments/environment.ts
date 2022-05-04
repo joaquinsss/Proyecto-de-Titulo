72a3b0d8572e3128f3232b5ddc432b446c9d3f7b
joaquinsss <joaquinsalas13@gmail.com>
2022-05-03 22:16:35 -0400

    Lo-gin sprint 1 v1.1.0

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+
+
+
+export const environment = {
+  production: false,
+  firebase: {
+  
+    apiKey: "AIzaSyAVBuAfRZf3uUHI14KbnWRBGOhdxdWhsOw",
+    authDomain: "angular-login-4670a.firebaseapp.com",
+    projectId: "angular-login-4670a",
+    storageBucket: "angular-login-4670a.appspot.com",
+    messagingSenderId: "554953473298",
+    appId: "1:554953473298:web:12ce32f1823049016bc7cb",
+    measurementId: "G-G809SL635H"
+    
+  },
+
+};
+
